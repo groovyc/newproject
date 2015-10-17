5e80fcd0f576c6d696c8127eb1fe664a9aaac6f3
groovyc <garveymusic4@gmail.com>
2015-10-17 14:38:04 +0100

    mobileserviceadded

---
+++ fourchordprojectService/Controllers/BillboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace fourchordprojectService.Controllers
+{
+    public class BillboardController : ApiController
+    {
+        public ApiServices Services { get; set; }
+
+        // GET api/Billboard
+        public string Get()
+        {
+            Services.Log.Info("Hello from custom controller!");
+            return "Hello";
+        }
+
+    }
+}
---
+++ fourchordprojectService/DataObjects/Song.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fourchordprojectService.DataObjects
+{
+    public class Song
+    {
+    }
+}
